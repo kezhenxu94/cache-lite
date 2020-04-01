f396151eedea669d6e854edd8275d691b7737838
kezhenxu94 <kezhenxu94@163.com>
2020-04-01 23:14:13 +0800

    Set up maven wrapper

--- src/test/kotlin/io/github/kezhenxu94/cache/lite/impl/SoftCacheTest.kt
+++ src/test/kotlin/io/github/kezhenxu94/cache/lite/impl/SoftCacheTest.kt
@@ -10,21 +10,21 @@ import org.junit.Test
  * @author kezhenxu94 (kezhenxu94 at 163 dot com)
  */
 class SoftCacheTest : BaseCacheTest() {
-	init {
-		cache = SoftCache(PerpetualCache())
-	}
+  init {
+    cache = SoftCache(PerpetualCache())
+  }
 
-	@Test
-	fun shouldClearUnreachableItems() {
-		val size = 2048
-		for (i in 0 until size) {
-			cache[i] = ByteArray(ONE_MEGABYTE)
-		}
-		System.gc()
-		Assert.assertTrue(cache.size < size)
-	}
+  @Test
+  fun shouldClearUnreachableItems() {
+    val size = 2048
+    for (i in 0 until size) {
+      cache[i] = ByteArray(ONE_MEGABYTE)
+    }
+    System.gc()
+    Assert.assertTrue(cache.size < size)
+  }
 
-	companion object {
-		private const val ONE_MEGABYTE = 1024 * 1024
-	}
+  companion object {
+    private const val ONE_MEGABYTE = 1024 * 1024
+  }
 }
